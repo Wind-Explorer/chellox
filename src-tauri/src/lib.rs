09a98edfeedf3e953b12e3115e33d7c75c003f76
Wind-Explorer <windexplorer@outlook.com>
2025-07-14 18:51:17 +0800

    added some file reading

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,44 @@
+use std::{
+    collections::VecDeque,
+    fs::{self, File},
+    io::{BufRead, BufReader},
+};
+
+#[tauri::command]
+#[specta::specta]
+fn read_file_by_path(path: String) -> Result<String, String> {
+    fs::read_to_string(path).map_err(|e| e.to_string())
+}
+
+#[tauri::command]
+#[specta::specta]
+fn read_last_lines(path: String, num_lines: u32) -> Result<Vec<String>, String> {
+    bufreader_last_lines(&path, num_lines as usize).map_err(|e| e.to_string())
+}
+
+fn bufreader_last_lines(path: &str, num_lines: usize) -> std::io::Result<Vec<String>> {
+    let file = File::open(path)?;
+    let reader = BufReader::new(file);
+    let mut lines = VecDeque::with_capacity(num_lines);
+
+    for line in reader.lines() {
+        let line = line?;
+        if lines.len() == num_lines {
+            lines.pop_front();
+        }
+        lines.push_back(line);
+    }
+
+    Ok(lines.into_iter().collect())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     let builder =
-        tauri_specta::Builder::<tauri::Wry>::new().commands(tauri_specta::collect_commands![]);
+        tauri_specta::Builder::<tauri::Wry>::new().commands(tauri_specta::collect_commands![
+            read_file_by_path,
+            read_last_lines
+        ]);
     #[cfg(debug_assertions)]
     builder
         .export(
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,7 +1,54 @@
+import { Button, Input, NumberInput } from "@heroui/react";
+import { useState } from "react";
+import { commands } from "../../bindings";
+
 export default function HomePage() {
+  const [userInput, setUserInput] = useState("");
+  const [numLines, setNumLines] = useState<number | undefined>(undefined);
+  const [submissionResponse, setSubmissionResponse] = useState("");
+
+  const handleSubmit = async () => {
+    if (numLines !== undefined && numLines !== null && numLines > 0) {
+      const result = await commands.readLastLines(userInput, numLines);
+      if (result.status === "ok") {
+        setSubmissionResponse(result.data.join("\n"));
+      } else {
+        setSubmissionResponse(`Error: ${result.error}`);
+      }
+    } else {
+      const result = await commands.readFileByPath(userInput);
+      if (result.status === "ok") {
+        setSubmissionResponse(result.data);
+      } else {
+        setSubmissionResponse(`Error: ${result.error}`);
+      }
+    }
+  };
+
   return (
-    <div className="p-6">
-      <p className="text-3xl font-bold">Home</p>
+    <div className="p-4 h-full flex flex-col gap-4">
+      <div className="flex gap-4 items-center">
+        <Input
+          label="Path to log file"
+          value={userInput}
+          onValueChange={setUserInput}
+        />
+        <NumberInput
+          value={numLines}
+          onValueChange={setNumLines}
+          min={1}
+          label="Last N lines (optional)"
+          className="min-w-12"
+        />
+        <Button size="lg" color="primary" onPress={handleSubmit}>
+          Submit
+        </Button>
+      </div>
+      <textarea
+        className="size-full p-4 bg-default-500/10 rounded-xl shadow-small"
+        value={submissionResponse}
+        onChange={(e) => setSubmissionResponse(e.target.value)}
+      />
     </div>
   );
 }
